a67d2bbee65e2fc0cb7aa506a55e87ede6754ee9
Matvey Vasilev <matt-14@mail.ru>
2019-10-04 20:05:18 +0300

    vers1

---
+++ ParallelLaba2/ParallelLaba2/Sum.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+using System.Threading;
+namespace ParallelLaba1
+{
+    class Sum
+    {
+        static readonly Random _rnd = new Random();
+        private int result;
+        private int[] a;
+        public Sum (int[] a)
+        {
+            this.a = a;
+        }
+        public void sumValues(int id1, int id2)
+        {
+            for (int i = id1; i <= id2; i++)
+                result += a[i];
+            var workTime = _rnd.Next(1500, 5500);
+            Thread.Sleep(workTime);
+        }
+        public int getResult()
+        {
+            return result;
+        }
+    }
+}
